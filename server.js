104bddd8789f01bc3e8939a3b265b87b4a1281e1
Samuel Garbo <samuel.garbo@gmail.com>
2020-07-08 17:58:13 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log('App is running on port ' + PORT))
+
+
+
