bbc2a9426621b598716b5d980823a0f8c8ef83f2
Samuel Garbo <samuel.garbo@gmail.com>
2020-08-28 12:32:16 +0300

    Layout for Recipe, added RecipeTable

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,18 +1,140 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import RecipeTable from "./RecipeTable";
+
+const method = [
+  "Sequi aut iusto nisi possimus incidunt ut. Illo qui rerum cupiditate ut omnis cumque et dolore. Corporis et delectus quo.",
+  "Est ducimus modi nihil debitis quisquam eius officiis quis. Omnis reprehenderit ipsum ea consequatur quas ab dolorem minus veniam. Assumenda iste dicta nobis fugit. Tempore explicabo officia earum laborum.",
+  "In quaerat voluptatem quis adipisci nihil eos. Doloremque et recusandae vitae incidunt occaecati ut. Earum ut aperiam eaque rem qui eveniet doloremque. Et neque et reiciendis blanditiis in fugit rerum sunt. Ipsa quae ut sunt corrupti atque impedit. Vitae sed nesciunt quis rerum est unde modi quibusdam necessitatibus.",
+];
 
 const useStyles = makeStyles((theme) => ({
-  root: {   
- 
+  root: {
+    maxWidth: theme.breakpoints.values.md,
+    margin: "auto",
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    paddingTop: theme.spacing(1),
+    [theme.breakpoints.down("xs")]: {
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+    },
   },
-  title:{
-   
-  }
+  title: {
+    padding: theme.spacing(2),
+    [theme.breakpoints.down("xs")]: {
+      padding: theme.spacing(1),
+    },
+  },
+  imageGrid: {
+    textAlign: "right",
+    [theme.breakpoints.down("xs")]: {
+      textAlign: "center",
+    },
+  },
+  image: {
+    borderRadius: "5px",
+  },
+  totalTime: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      paddingLeft: theme.spacing(4),
+    },
+  },
+  yield: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      paddingLeft: theme.spacing(4),
+    },
+  },
+  ingredients: {
+    [theme.breakpoints.up("md")]: {
+      marginLeft: theme.spacing(10),
+    },
+  },
+  method: {},
+  imgContainer: {
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+      textAlign: "center",
+    },
+  },
+  textContainer: {
+    padding: theme.spacing(2),
+    [theme.breakpoints.only("sm")]: {
+      padding: theme.spacing(4),
+    },
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
 }));
+function IngredientList({ ingredientLines }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs container direction="column" className={classes.ingredients}>
+      <Typography variant="h6" gutterBottom>
+        Ingredients:
+      </Typography>
+      <List dense>
+        {ingredientLines.map((ingredient, idx) => (
+          <ListItem key={idx}>
+            <ListItemText primary={ingredient} />
+          </ListItem>
+        ))}
+      </List>
+    </Grid>
+  );
+}
+function MethodList({ method }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs className={classes.method}>
+      <Typography variant="h6" gutterBottom>
+        Method:
+      </Typography>
+      <List dense>
+        {method.map((instruction, idx) => (
+          <ListItem key={idx}>
+            <ListItemText primary={instruction} />
+          </ListItem>
+        ))}
+      </List>
+    </Grid>
+  );
+}
+function Yield({ recipe }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs className={classes.yield}>
+      <Typography variant="subtitle2">Yield:</Typography>
+      <Typography variant="body2">{recipe["yield"]}</Typography>
+    </Grid>
+  );
+}
+function PreparationTime({ totalTime }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs className={classes.totalTime}>
+      <Typography variant="subtitle2">Preparation:</Typography>
+      <Typography variant="body2">{totalTime}</Typography>
+    </Grid>
+  );
+}
+function FoodImage({ image, label }) {
+  const classes = useStyles();
+  return (
+    <Grid item xs className={classes.imageGrid}>
+      <img src={image} alt={label} className={classes.image} />
+    </Grid>
+  );
+}
 function Recipe(props) {
   const { recipes } = props;
   const { id } = useParams();
@@ -25,7 +147,7 @@ function Recipe(props) {
       .replace(/[()]/g, "")
       .toLowerCase();
     return name === id;
-  });
+  })[0].recipe;
 
   const {
     label,
@@ -33,32 +155,36 @@ function Recipe(props) {
     image,
     ingredientLines,
     totalNutrients,
-  } = recipe[0].recipe;
+    totalWeight,
+  } = recipe;
   return (
-    <Container maxWidth="lg">
-      <Grid container direction="column" alignItems="center">
-        <Grid item xs>
-          <Typography variant="h4" component="h1" align='center' className={classes.title}>
-            {label}
-          </Typography>
-        </Grid>
-        <Grid item xs >
-          <img src={image} />
-        </Grid>
-        <Grid item xs>
-          Total time: {totalTime}
-        </Grid>
-        <Grid item xs>
-          Yield: {recipe[0].recipe["yield"]}
-        </Grid>
-        <Grid item xs>
-          Ingredients:{" "}
-          {ingredientLines.map((ingredient) => (
-            <p>{ingredient}</p>
-          ))}
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      className={classes.root}
+    >
+      {/* Recipe title */}
+      <Grid item xs className={classes.title}>
+        <Typography variant="h4" component="h1" align="center" gutterBottom>
+          {label}
+        </Typography>
+      </Grid>
+      {/* Food image, preparation time and portions */}
+      <Grid item container classes={{ container: classes.imgContainer }}>
+        <FoodImage image={image} label={label} />
+        <Grid item xs container direction="column">
+          <PreparationTime totalTime={totalTime} />
+          <Yield recipe={recipe} />
         </Grid>
       </Grid>
-    </Container>
+      {/* Ingredients and method */}
+      <Grid item xs container classes={{ container: classes.textContainer }}>
+        <IngredientList ingredientLines={ingredientLines} />
+        <MethodList method={method} />
+      </Grid>
+      <RecipeTable totalNutrients={totalNutrients} totalWeight={totalWeight} />
+    </Grid>
   );
 }
 
---
+++ client/src/components/RecipeTable.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  table: {
+    maxWidth: "80%",
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+function roundToDecimal(num) {
+  return Math.round((num + Number.EPSILON) * 10) / 10;
+}
+const rows = [createData("Frozen yoghurt", 159, 6.0, 24, 4.0)];
+
+export default function DenseTable({ totalNutrients, totalWeight }) {
+  const classes = useStyles();
+  const { CHOCDF, ENERC_KCAL, FAT, PROCNT, FIBTG } = totalNutrients;
+
+  const for100Grams = 100 / totalWeight;
+  let nutrients = [ENERC_KCAL, CHOCDF, FAT, PROCNT, FIBTG];
+  nutrients = nutrients.map((val) => {
+    let newQuantity = val.quantity * for100Grams;
+    return { ...val, quantity: newQuantity };
+  });
+  return (
+    <Grid item xs container justify="center">
+      <Table
+        className={classes.table}
+        size="small"
+        aria-label="nutrition values"
+      >
+        <TableHead>
+          <TableRow>
+            {nutrients.map((nutrient) => (
+              <TableCell key={nutrient.label} align="right">
+                {nutrient.label}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            {nutrients.map((nutrient) => (
+              <TableCell key={nutrient.label} align="right">{`${roundToDecimal(
+                nutrient.quantity
+              )}${nutrient.unit}`}</TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Grid>
+  );
+}
